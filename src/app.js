988c57655c17623a0270b93ac22a1a8cf45ea133
Atharva Mhatre <89691657+Atharvamhatre149@users.noreply.github.com>
2025-08-04 00:58:18 +0530

    cookie fix

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Set-Cookie']
 }));
 
 app.use(express.json({limit:"16kb"}));
