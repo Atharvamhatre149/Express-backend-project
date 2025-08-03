194020ccc4ea668cfb2041da3f609428f3aee390
Atharva Mhatre <89691657+Atharvamhatre149@users.noreply.github.com>
2025-08-03 23:29:00 +0530

    update files for deploy

--- src/routers/healthCheck.routes.js
+++ src/routers/healthCheck.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
-
-import healthcheck from "../controllers/healthCheck.controller.js";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import healthcheck from "../controllers/healthcheck.controller.js";
 
 const router=Router();
 
