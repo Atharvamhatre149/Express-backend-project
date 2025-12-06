073bc988d50e349001fea1a4404070f2ff0d9ac0
Atharva Mhatre <89691657+Atharvamhatre149@users.noreply.github.com>
2025-12-06 11:40:00 +0530

    Fix duplicate file name

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,29 @@
-import multer from "multer";
+import fs from 'fs';
+import path from 'path';
+import multer from 'multer';
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
+  destination: function (req, file, cb) {
+    cb(null, './public/temp');
+  },
+  filename: function (req, file, cb) {
+    const uploadDir = './public/temp';
+    const originalName = file.originalname;
+
+    const ext = path.extname(originalName);         // ".jpg"
+    const name = path.basename(originalName, ext);  // "IMG_1004"
+
+    const fullPath = path.join(uploadDir, originalName);
+
+    let finalName = originalName;
+
+    // If file already exists ONCE → add _1
+    if (fs.existsSync(fullPath)) {
+      finalName = `${name}_1${ext}`;
     }
-  })
-  
-  export const upload = multer( { storage })
+
+    cb(null, finalName);
+  }
+});
+
+export const upload = multer({ storage });
